713ad73081dcd5ea07a20f449ffc1c8e7167b3a9
Jim Doyle <indellibleink@gmail.com>
2018-05-21 18:53:59 -0400

    updated files

--- scripts/weather_report.js
+++ scripts/weather_report.js
@@ -1,4 +1,10 @@
 // script page
 // Get location
 
-var location=document.form(['searchForm'].querySelector('input'));
+ // request: https://fcc-weather-api.glitch.me/api/current?lat=41.87092932&lon=-71.42788283
+ // request: https://fcc-weather-api.glitch.me/api/current?lat=41.87&lon=-71.43
+ // weather[0].icon
+ //{"coord":{"lon":-71.43,"lat":41.87}
+ // Date///
+
+ 
